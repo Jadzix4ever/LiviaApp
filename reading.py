82c83239dfd7b1678ce87d1c442383f285ff95ab
Lukasz Teresak <lteresak@yahoo.com>
2024-01-03 17:56:24 +0100

    Slider created

--- reading.py
+++ reading.py
@@ -21,13 +21,15 @@ class Reading:
         self.save_book_arrangement(book_name)
         self.dictionary = dictionary
         self.file_path = file_path
-        self.next_side()
 
-        slider = tk.Scale(self.top, from_=1, to=len(self.book), orient=tk.HORIZONTAL, command=self.on_slider_change, length=300)
-        slider.pack(side='bottom')
+        self.slider = tk.Scale(self.top, from_=1, to=len(self.book), orient=tk.HORIZONTAL,
+                               command=self.on_slider_change, length=300)
+        self.slider.pack(side='bottom')
         frame = create_frame(self.top, 'bottom')
         create_button(frame, 'Previous', self.previous_side, 'pack', {'side': 'left'})
         create_button(frame, 'Next', self.next_side, 'pack', {'side': 'left'})
+
+        self.next_side()
 
     def label_calculator(self):
         page = 1
@@ -52,38 +54,31 @@ class Reading:
                 self.book[page] = []
             label.destroy()
 
-    def previous_side(self):
-        if self.page == 1:
-            return
-        self.page -= 1
-        self.clear_labels()
-        pairs = word_and_sentence(self.book, self.page)
-        number = 0
-
-        for word in self.book[str(self.page)]:
-            label = create_label(self.top, pairs[number][0], 'Arial, 14', None, 'place',
-                                 {'x': word[1], 'y': word[2]})
-            label.bind("<Button-1>", lambda event,
-                       question=pairs[number][0],
-                       sentence=pairs[number][1]:
-                       FlashcardsInputDialog(self.top, self.dictionary, self.file_path, question, sentence))
-            number += 1
-
     def clear_labels(self):
         for widget in self.top.winfo_children():
             if isinstance(widget, tk.Label):
                 widget.destroy()
 
     def on_slider_change(self, value):
-        self.page = int(value) - 1
-        self.next_side()
-        pass
+        self.page = int(value)
+        self.to_turn_page()
 
     def next_side(self):
         if len(self.book) == self.page:
             return
 
         self.page += 1
+        self.to_turn_page()
+        self.slider.set(self.page)
+
+    def previous_side(self):
+        if self.page == 1:
+            return
+        self.page -= 1
+        self.to_turn_page()
+        self.slider.set(self.page)
+
+    def to_turn_page(self):
         self.clear_labels()
         pairs = word_and_sentence(self.book, self.page)
         number = 0
@@ -91,9 +86,7 @@ class Reading:
         for word in self.book[str(self.page)]:
             label = create_label(self.top, pairs[number][0], 'Arial, 14', None, 'place',
                                  {'x': word[1], 'y': word[2]})
-            label.bind("<Button-1>", lambda event,
-                       question=pairs[number][0],
-                       sentence=pairs[number][1]:
+            label.bind("<Button-1>", lambda event, question=pairs[number][0], sentence=pairs[number][1]:
                        FlashcardsInputDialog(self.top, self.dictionary, self.file_path, question, sentence, pairs))
             number += 1
 
