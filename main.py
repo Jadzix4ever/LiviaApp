d2c495cf00fdae8d871562859b283577f210c8b2
Lukasz Teresak <lteresak@yahoo.com>
2024-01-20 13:06:36 +0100

    Standard translation AI supported

--- main.py
+++ main.py
@@ -5,8 +5,3 @@ if __name__ == "__main__":
     root = tkinter.Tk()
     LiviaApp(root)
     root.mainloop()
-
-# TODO: Add an basic translation option.
-# TODO: Testing of installation process on other devices.
-# TODO: Use inheritance at other windows else main window.
-# TODO: Add an exception in the label_calculator function if the waiting time is over 1s.
